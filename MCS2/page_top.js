c1fde37dac6978f66666b2b1063d3db770442443
XapiMa <k905672@kansai-u.ac.jp>
2016-02-03 08:56:08 +0900

    ページ頭への移動にアニメーション。スクロールの合わせてボタンの出現。 imgディレクトリに画像ファイル移動

---
+++ MCS2/page_top.js
@@ -0,0 +1,15 @@
+$(function() {
+    var pageTop = $('.page_top');
+    pageTop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            pageTop.fadeIn();
+        } else {
+            pageTop.fadeOut();
+        }
+    });
+    pageTop.click(function () {
+        $('body, html').animate({scrollTop:0}, 500, 'swing');
+        return false;
+    });
+});
