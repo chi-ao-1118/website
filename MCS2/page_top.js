d1666c19d3cee57b6c9b45cc865d89a7a46a4dc6
XapiMa <k905672@kansai-u.ac.jp>
2016-02-09 22:22:27 +0900

    スマホ版でpage_top非表示 640px直前でブラウザ幅がおかしくなる。paddingとwidthの同時利用が原因？

--- MCS2/page_top.js
+++ MCS2/page_top.js
@@ -1,12 +1,14 @@
-$(function() {
+$(window).on('load resize', function() {
     var pageTop = $('.page_top');
     pageTop.hide();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            pageTop.fadeIn();
-        } else {
-            pageTop.fadeOut();
-        }
+		if($(window).width()>=640){
+			if ($(this).scrollTop() > 200) {
+				pageTop.fadeIn();
+			} else {
+				pageTop.fadeOut();
+			}
+		}
     });
     pageTop.click(function () {
         $('body, html').animate({scrollTop:0}, 500, 'swing');
