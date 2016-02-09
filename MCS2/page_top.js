09cd4eecb07751d510f9dba3da6cbf34cfd663ac
XapiMa <k905672@kansai-u.ac.jp>
2016-02-09 23:13:09 +0900

    page_topの画面にサイズによる分岐の再構成

--- MCS2/page_top.js
+++ MCS2/page_top.js
@@ -1,17 +1,21 @@
-$(window).on('load resize', function() {
-    var pageTop = $('.page_top');
-    pageTop.hide();
-    $(window).scroll(function () {
-		if($(window).width()>=640){
-			if ($(this).scrollTop() > 200) {
-				pageTop.fadeIn();
-			} else {
-				pageTop.fadeOut();
+$(function(){
+	$(window).on('load resize', function() {
+		var pageTop = $('.page_top');
+		pageTop.hide();
+		$(window).scroll(function () {
+			if($(window).width()>=640){
+				if ($(this).scrollTop() > 200) {
+					pageTop.fadeIn();
+				} else {
+					pageTop.fadeOut();
+				}
+			}else{
+				pageTop.hide();
 			}
-		}
-    });
-    pageTop.click(function () {
-        $('body, html').animate({scrollTop:0}, 500, 'swing');
-        return false;
-    });
+		});
+	});
+	pageTop.click(function () {
+		$('body, html').animate({scrollTop:0}, 500, 'swing');
+		return false;
+	});
 });
