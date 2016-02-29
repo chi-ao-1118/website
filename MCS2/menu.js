fce427af9be2b18a45467a560ed8139d092717a4
XapiMa <k905672@kansai-u.ac.jp>
2016-02-29 13:24:14 +0900

    ハンバーガーメニュー時に他のコンテンツの排除

--- MCS2/menu.js
+++ MCS2/menu.js
@@ -6,16 +6,36 @@ $(".menu").click(function(){
 	$(this).data("click", ++num);
     var click = $(this).data("click");
     if(click%2===1){
-		$(".hamMenu").fadeIn("slow");
-		$(".menu_overlay").fadeIn("slow");
+		$(".hamMenu").show();
+		$(".menu_overlay").show();
+		$(".topImg").hide();
+		$(".mainWrapper").hide();
+		$(".topLost").hide();
 	}else{
-		$(".hamMenu").fadeOut("slow");
-		$(".menu_overlay").fadeOut("slow");
+		$(".hamMenu").hide();
+		$(".menu_overlay").hide();
+		$(".topImg").show();
+		$(".mainWrapper").show();
+		$(".topLost").show();
 	}
 });
+
 $(".menu_overlay").click(function(){
 	$(".menu").data("click", ++num);
     var click = $(this).data("click");
-	$(".hamMenu").fadeOut("slow");
-	$(".menu_overlay").fadeOut("slow");
+	$(".hamMenu").hide("slow");
+	$(".menu_overlay").hide("slow");
+});
+
+$(window).on('load resize', function() {
+	if ( !window.innerWidth ){
+		return;
+	}
+	if ( window.innerWidth >640 ){
+		$(".hamMenu").hide();
+		$(".menu_overlay").hide();
+		$(".topImg").show();
+		$(".mainWrapper").show();
+		$(".topLost").show();
+	}
 });
