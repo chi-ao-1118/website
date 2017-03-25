e6be843d07faeb6553d7feeaa7247988ae182ae7
k905672 <k905672@kansai-u.ac.jp>
2017-03-26 03:02:26 +0900

    bkBtnとheaderのリファクタリングと外部読み込みの実装．pressreleaseをxmlからパースする手法の採用．

--- MCS2/js/menu.js
+++ MCS2/js/menu.js
@@ -3,43 +3,44 @@
 
 // メニューの読み込み
 $(function() {
-    $("#hamMenu").load("hamMenu.html");
-    $("#foot").load("footer.html");
+    $("#header").load("header.html", add_this);
+    $("#hamMenu").load("hamMenu.html", menu_on);
+    $("#footer").load("footer.html");
+    $("#bkBtn").load("bkBtn.html", add_bk_id);
 });
 
+// loadは非同期に実行されるため，普通に実行すると思い通りの結果にならない
+// 確実にload後に実行されるようにcallback関数として定義する
+var add_this = function() {
+    $(header_id).addClass("this");
+};
+var add_bk_id = function() {
+    $(".bkAnime").attr('id', bk_id);
+};
 //	ハンバーガーメニューを出現させるクリックイベント
+// これもcallback関数とする必要がある
 var num = 0;
-$(".menu").click(function() {
-    $(this).data("click", ++num);
-    var click = $(this).data("click");
-    if (click % 2 === 1) {
-        $("#hamMenu").show();
-        $(".menu_overlay").show();
-        $(".topImg").hide();
-        $(".mainWrap").hide();
-        $(".topLost").hide();
-        $(".works_bkBtnWrap").hide();
-        $("#top_firstView").attr('id', "none");
-    } else {
-        $("#hamMenu").hide();
-        $(".menu_overlay").hide();
-        $(".topImg").show();
-        $(".mainWrap").show();
-        $(".topLost").show();
-        $(".works_bkBtnWrap").show();
-        $("#none").attr('id', "top_firstView");
-    }
-});
-/*
-$(".menu_overlay").click(function(){
-	$(".menu").data("click", ++num);
-    var click = $(this).data("click");
-		$(".hamMenu").hide();
-		$(".menu_overlay").hide();
-		$(".topImg").show();
-		$(".mainWrap").show();
-		$(".topLost").show();
-});*/
+var menu_on = function() {
+    $(".menu").click(function() {
+        $(this).data("click", ++num);
+        var click = $(this).data("click");
+        if (click % 2 === 1) {
+            $("#hamMenu").show();
+            $(".menu_overlay").show();
+            $(".topImg").hide();
+            $(".mainWrap").hide();
+            $(".hamLost").hide();
+            $("#top_firstView").attr('id', "none");
+        } else {
+            $("#hamMenu").hide();
+            $(".menu_overlay").hide();
+            $(".topImg").show();
+            $(".mainWrap").show();
+            $(".hamLost").show();
+            $("#none").attr('id', "top_firstView");
+        }
+    });
+};
 
 $(window).on('load resize', function() {
     if (!window.innerWidth) {
@@ -50,6 +51,6 @@ $(window).on('load resize', function() {
         $(".menu_overlay").hide();
         $(".topImg").show();
         $(".mainWrap").show();
-        $(".topLost").show();
+        $(".hamLost").show();
     }
 });
