177ffad8233366d25c0e17a1e91237034c10e8c0
k905672 <k905672@kansai-u.ac.jp>
2017-04-08 00:46:26 +0900

    shortfilmのxmlパースを作成途中。&マークがxmlに混入するとエラーが出る

--- MCS2/js/contents.js
+++ MCS2/js/contents.js
@@ -1,18 +1,18 @@
 $(function() {
-    $.ajax({
-        url: 'contents.xml',
-        dataType: 'xml',
-        success: function(data) {
+  $.ajax({
+    url: 'contents.xml',
+    dataType: 'xml',
+    success: function(data) {
 
-            var insertContents = '';
-            $('originalContents item', data).each(function() {
-                var thisItem = $(this);
-                var link = thisItem.children('link').text();
-                var img = thisItem.children('img').text();
-                var title = thisItem.children('title').text();
-                var article = thisItem.children('article').text();
+      var insertContents = '';
+      $('originalContents item', data).each(function() {
+        var thisItem = $(this);
+        var link = thisItem.children('link').text();
+        var img = thisItem.children('img').text();
+        var title = thisItem.children('title').text();
+        var article = thisItem.children('article').text();
 
-                insertContents += `
+        insertContents += `
                 <article class="original">
                   <a href="${link}" target="_blank">
                     <figure class="lostFig">
@@ -24,8 +24,35 @@ $(function() {
                   </figure>
                 </a>
                 </article>`;
-              });
-            $('#originalItem').append(insertContents);
-        }
-    });
+      });
+      $('#originalItem').append(insertContents);
+    }
+  });
+  // $.ajax({
+  //   url: 'shortfilm.xml',
+  //   dataType: 'xml',
+  //   success: function(data) {
+  //
+  //     var insertContents = '';
+  //     $('item', data).each(function() {
+  //       var thisItem = $(this);
+  //       var src = thisItem.children('src').text();
+  //       var link = thisItem.children('link').text();
+  //       var title = thisItem.children('title').text();
+  //       var article = thisItem.children('article').text();
+  //
+  //       insertContents += `
+  //       <article>
+  //         <div class="flexIframe filmMovie">
+  //           <iframe src="${src}" frameborder="0" allowfullscreen></iframe>
+  //         </div>
+  //         <a href="${link}" target="_blank">
+  //           <h1>${title}</h1>
+  //           <p>${article}</p>
+  //         </a>
+  //       </article>`;
+  //     });
+  //     $('#shortfilm').append(insertContents);
+  //   }
+  // });
 });
