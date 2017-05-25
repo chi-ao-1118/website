58b0605e153697c4ada25813f5940fbe8e51e40c
Naoki Harima <k905672@kansai-u.ac.jp>
2017-05-25 18:49:58 +0900

    bodyの長さを十分にとってください

--- MCS2/cf2017/js/add_fixed.js
+++ MCS2/cf2017/js/add_fixed.js
@@ -1,40 +1,44 @@
-$(window).load(function () {
+$(window).load(function() {
 
-    var studio = $('#studio'), studio_offset = studio.offset();
-    var  live = $('#live'), live_offset = live.offset();
-    var  shop = $('#shop'), shop_offset = shop.offset();
-    var d_studio = $("#d_studio"), d_studio_offset = d_studio.offset();
-    var d_live = $("#d_live"), d_live_offset = d_live.offset();;
+  var studio = $('#studio'),
+    studio_offset = studio.offset();
+  var live = $('#live'),
+    live_offset = live.offset();
+  var shop = $('#shop'),
+    shop_offset = shop.offset();
+  var top = $("#Top");
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > studio_offset.top) {
-            add(studio);
-        } else {
-            remove(studio);
-        }
-        if ($(window).scrollTop() > d_studio_offset.top) {
-            remove(studio);
-            add(live);
-        } else {
-            remove(live);
-        }
-        if ($(window).scrollTop() > live_offset.top) {
-            remove(live);
-            add(shop);
-        } else {
-            remove(shop);
-        }
-    });
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > studio_offset.top) {
+      remove(top);
+      add(studio);
+    } else {
+      remove(studio);
+      top.css("position","fixed");
+    }
+    if ($(window).scrollTop() > live_offset.top) {
+      remove(studio);
+      add(live);
+    } else {
+      remove(live);
+    }
+    if ($(window).scrollTop() > shop_offset.top) {
+      remove(live);
+      add(shop);
+    } else {
+      remove(shop);
+    }
+  });
 });
 
-function add(obj){
-  obj.css("position","fixed");
-  obj.css("overflow","scroll");
-  obj.css("top","0");
-  obj.css("left","0");
+function add(obj) {
+  obj.css("position", "fixed");
+  obj.css("overflow", "scroll");
+  obj.css("top", "0");
+  obj.css("left", "0");
 };
 
-function remove(obj){
-    obj.css("position","relative");
-    obj.css("overflow","hidden");
+function remove(obj) {
+  obj.css("position", "relative");
+  obj.css("overflow", "hidden");
 };
