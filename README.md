7ca64cf63d20d8424ba8771757764e19ad3eeefb
tanakura <deric178431@ezweb.ne.jp>
2017-07-07 12:03:55 +0000

    itimai

--- README.md
+++
@@ -1,5 +0,0 @@
-# website
-このページは[MCSのホームページ](http://mcs-kutc.com/)のためのページです
-
-
-[cacoo](https://cacoo.com/diagrams/6FgsdY1DrKOhyciw)
---
+++ test/contents-class/js/contents.js
@@ -0,0 +1,59 @@
+$(function() {
+  $.ajax({
+    url: 'contents.xml',
+    dataType: 'xml',
+    success: function(data) {
+
+      var insertContents = '';
+      $('originalContents item', data).each(function() {
+        var thisItem = $(this);
+        var link = thisItem.children('link').text();
+        var img = thisItem.children('img').text();
+        var title = thisItem.children('title').text();
+        var article = thisItem.children('article').text();
+        var className = thisItem.children('class').text();
+
+        insertContents += `
+                <article class="${className} original">
+                  <a href="${link}" target="_blank">
+                    <figure class="lostFig">
+                      <img src="img/contents/PC/${img}">
+                      <figcaption>
+                        <h1>${title}</h3>
+                      <p>${article}</p>
+                    </figcaption>
+                  </figure>
+                </a>
+                </article>`;
+      });
+      $('#originalItem').append(insertContents);
+    }
+  });
+  // $.ajax({
+  //   url: 'shortfilm.xml',
+  //   dataType: 'xml',
+  //   success: function(data) {
+  //
+  //     var insertContents = '';
+  //     $('item', data).each(function() {
+  //       var thisItem = $(this);
+  //       var src = thisItem.children('src').text();
+  //       var link = thisItem.children('link').text();
+  //       var title = thisItem.children('title').text();
+  //       var article = thisItem.children('article').text();
+  //
+  //       insertContents += `
+  //       <article>
+  //         <div class="flexIframe filmMovie">
+  //           <iframe src="${src}" frameborder="0" allowfullscreen></iframe>
+  //         </div>
+  //         <a href="${link}" target="_blank">
+  //           <h1>${title}</h1>
+  //           <p>${article}</p>
+  //         </a>
+  //       </article>`;
+  //     });
+  //     $('#shortfilm').append(insertContents);
+  //   }
+  // });
+});
