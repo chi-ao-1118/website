0d9f7fc0d82099b1daaaefeb3dc20697bab0d212
takeda123 <58212908+takeda123@users.noreply.github.com>
2020-03-04 23:58:52 +0900

    変更
    デザインやリンクなどの修正

--- MCS3/special/recruit_2020/main.js
+++ MCS3/special/recruit_2020/main.js
@@ -31,5 +31,37 @@ $(function(){
 
   });
 
+  // ボタンスクロール
+  $('a[href^="#"]').click(function(){
+    var speed = 600;
+    var href= $(this).attr("href");
+    var target = $(href == "#how-to" || href == "id=how-to" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+  $('a[href^="#"]').click(function(){
+    var speed = 600;
+    var href= $(this).attr("href");
+    var target = $(href == "#detail" || href == "id=detail" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+
+  $('a[href^="#"]').click(function(){
+    var speed = 600;
+    var href= $(this).attr("href");
+    var target = $(href == "#qa" || href == "id=qa" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+
+
 
   });
+
